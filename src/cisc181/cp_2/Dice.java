9007e364f029207f68d5fe5039a99d91bb22375a
zpice <zp2016@mail.dlut.edu.cn>
2019-05-09 20:13:08 +0800

    comment

--- src/cisc181/cp_2/Dice.java
+++ src/cisc181/cp_2/Dice.java
@@ -6,7 +6,10 @@ import java.util.Random;
  * a class Dice server for game
  */
 public class Dice {
-
+    /**
+     * a public static method to imitate throw the dice
+     * @return 1~6 that player go
+     */
     public static int rollDice() {
         Random random = new Random();
         return random.nextInt(6) + 1;
